d5559c8d2a3cb52236310857a68db3e6d0955871
RafaelPastor67 <166877002+RafaelPastor67@users.noreply.github.com>
2025-01-30 17:11:01 -0300

    Update aula1_questao5.py

--- Módulo 5/aula1_questao5.py
+++ Módulo 5/aula1_questao5.py
@@ -1,6 +1,5 @@
 import emoji
 
-# Lista de emojis disponíveis
 emojis_disponiveis = {
     "❤️": ":red_heart:",
     "👍": ":thumbs_up:",
@@ -8,17 +7,13 @@ emojis_disponiveis = {
     "🥳": ":partying_face:"
 }
 
-# Apresenta os emojis disponíveis
 print("Emojis disponíveis:")
 for em, code in emojis_disponiveis.items():
     print(f"{em} - {code}")
 
-# Solicita uma frase codificada ao usuário
 frase_codificada = input("\nDigite uma frase e ela será emojizada:\n")
 
-# Emojiza a frase
 frase_emojizada = emoji.emojize(frase_codificada, use_aliases=True)
 
-# Apresenta a frase emojizada
 print("\nFrase emojizada:")
 print(frase_emojizada)
