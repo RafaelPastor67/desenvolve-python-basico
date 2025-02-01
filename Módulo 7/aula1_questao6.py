42a2b2f075f7da9aa80b30f8935755ca2a47087e
RafaelPastor67 <166877002+RafaelPastor67@users.noreply.github.com>
2025-02-01 01:55:49 -0300

    modulo 7

--- Módulo 7/aula1_questao6.py
+++ Módulo 7/aula1_questao6.py
@@ -1,3 +1,9 @@
-frase = input("Digite uma palavra: ")
-palavra = input("Digite uma palavra: ")
-
+frase = input("Digite uma frase: ")
+palavra = input("Digite uma palavra-objetivo: ")
+anagramas = []
+palavra_ordenada = sorted(palavra.lower())
+for i in frase.split():
+    sorted_i = sorted(i.lower())
+    if sorted_i == palavra_ordenada:
+        anagramas.append(i)
+print(anagramas)
--- Módulo 7/aula1_questao8.py
+++ Módulo 7/aula1_questao8.py
@@ -1,1 +1,40 @@
-#Não consegui
+cpf = input("Digite um CPF, na forma(XXX.XXX.XXX-XX): ")
+cpf = cpf.replace(".", "").replace("-", "")
+
+if cpf.isdigit() and len(cpf) == 11:
+    verificando_cpf = cpf[:-2]
+    
+    somatorio=0
+    verificador = 2
+    for digito in verificando_cpf[::-1]:
+        multiplicado = int(digito) * verificador
+        verificador += 1
+        somatorio += multiplicado
+    resto = somatorio % 11
+    if resto < 2:
+        prox = '0'
+    else:
+        prox = 11 - resto
+        prox = str(prox)
+        verificando_cpf += prox
+        
+
+    somatorio=0
+    verificador = 2
+    for digito in verificando_cpf[::-1]:
+        multiplicado = int(digito) * verificador
+        verificador += 1
+        somatorio += multiplicado
+    resto = somatorio % 11
+    if resto < 2:
+        prox = '0'
+    else:
+        prox = 11 - resto
+        prox = str(prox)
+        verificando_cpf += prox
+    if verificando_cpf == cpf:
+        print("CPF válido")
+    else:    
+        print("CPF inválido")
+else:
+    print("CPF inválido")
